e39d235bda5e88aadd5d475fe370d20da7e320db
elisa <elisa1603@gmail.com>
2020-06-17 21:50:47 -0500

    config enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://www.potterapi.com/v1',
+  apiKey:'$2a$10$wUKw54R0.lxF0Dx6qR0CVOse27HrHZjoq.2EEl.l0fUSOx3ZDcPem' 
 };
