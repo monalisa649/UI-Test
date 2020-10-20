e615472f20a9f4322ef32ee1bdf3149ce3177a43
monalisa649 <elisa1603@gmail.com>
2020-10-19 23:32:08 -0500

    Componente de busqueda

--- src/app/components/house/house.component.ts
+++ src/app/components/house/house.component.ts
@@ -27,7 +27,8 @@ export class HouseComponent implements OnInit {
 
      }
 
-
+// Variable for filterpipe
+ filterCharacters = '';
 
   ngOnInit(): void {
   }
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,19 +1,31 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { element } from 'protractor';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, arg: any): any {
-    if (arg === '' || arg.length < 3) return value;
-    const resultCharacter = [];
-    for (const character of value) {
-      if (character.name.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
-        resultCharacter.push(character);
-      };
-    };
-    return resultCharacter;
+  transform(value:any, arg: any): any {
+    if (arg === '' || arg.length < 3) {
+      return value;
+    }
+
+    arg =arg.toLowerCase();
+
+      return value.filter(element =>{
+        return element.name.toLowerCase()
+        .includes( arg)
+    });
+
+
+
+
+
+
+
+
+
   }
 
 }
